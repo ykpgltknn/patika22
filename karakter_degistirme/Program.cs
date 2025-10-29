d65e83b8fb1ce8f4e1068a1d327f6a4b71f9758e
Yakup Gültekin <124573071+ykpgltknn@users.noreply.github.com>
2025-10-29 16:04:20 +0300

    Add files via upload

---
+++ karakter_degistirme/Program.cs
@@ -0,0 +1,28 @@
+﻿﻿using System;
+
+namespace karakter_degistirme;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Baslangic:
+        Console.Write("\n Bir değer giriniz! \n ------------------ \n");
+        char[] chars = Console.ReadLine().ToCharArray();
+        Array.Reverse(chars);
+        Console.WriteLine(chars);
+
+        Secim:
+        Console.Write("\n Yeni değer girmek ister misiniz? \n E: Evet \n H: Hayır \n");
+        switch (Console.ReadLine().ToLower())
+        {
+            case "e":
+                goto Baslangic;
+            case "h":
+                break;
+            default:
+                Console.WriteLine("Geçersiz seçim yaptınız, lütfen size sunulan seçeneklerin dışına çıkmayın!");
+                goto Secim;
+        }
+    }
+}
